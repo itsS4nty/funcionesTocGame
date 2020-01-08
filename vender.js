ddbe91b1924d504e46d000693c033d32f4c6103c
santi <47889917+ezequielucho@users.noreply.github.com>
2020-01-08 14:47:12 +0100

    VentaYFuncionestxt

---
+++ vender.js
@@ -0,0 +1,17 @@
+function vender(cliente, tienda, tpv, ticket, importe){
+    var net = require('net');
+    var client = new net.Socket();
+    client.connect(8890, '127.0.0.1', function(){
+        console.log('Conectado al CoLinux');
+        var venta = `\x02${cliente};${tienda};${tpv};gleidy;${ticket};1;${importe};banco;PagoAplazado;;;;;`;
+        client.write(venta);
+    });
+    client.on('data', function(data){
+        console.log('Recibido: ' + data);
+        client.write('\x02ACK\x03');
+        client.destroy();
+    });
+    client.on('close', function(){
+        console.log('Conexión cerrada');
+    });
+}

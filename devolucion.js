080898174c9afff75d18137a4a6a06ef1e7187ad
santi <47889917+ezequielucho@users.noreply.github.com>
2020-01-08 17:20:43 +0100

    Update devolucion.js

--- devolucion.js
+++ devolucion.js
@@ -2,7 +2,7 @@ function devolucion(cliente, tienda, tpv, ticket, importe){
     var net = require('net');
     var client = new net.Socket();
     var current_datetime = new Date();
-    var fecha = ('0' + current_datetime.getDate()).slice(-2) + "-" + ('0' + current_datetime.getMonth()+1).slice(-2) + "-" + (current_datetime.getYear()-100);
+    var fecha = (('0' + current_datetime.getDate()).slice(-2) + "-" + ('0' + current_datetime.getMonth()+1).slice(-2) + "-" + (current_datetime.getYear()-100)).replace(/-/g, "");
     client.connect(8890, '127.0.0.1', function(){
         console.log('Conectado al CoLinux | Devolucion');
         var devolucion = `\x02${cliente};${tienda};${tpv};gleidy;${ticket};2;${importe};banco;;;;;NumOpOrg;${fecha}\x03`;
